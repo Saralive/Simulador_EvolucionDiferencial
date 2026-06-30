97a8f3af6fa5d36e0cc2c4ef0186df06a6861bf8
Saralive <sakusymphoni40@gmail.com>
2026-06-30 16:47:32 -0600

    Optimización de reconexión

--- ComplexNetworks-main/configEvolucionDiferencial.py
+++ ComplexNetworks-main/configEvolucionDiferencial.py
@@ -3,9 +3,6 @@
 N_CORRIDAS=1          # Número de veces que se ejecuta el genético
 N_GENERACIONES=0
 N_INDIVIDUOS=6        # El número de individuos debe ser par
-N_TORNEO=3            # Número de individuos seleccionados a torneo
-N_PADRES=2            # Número de padres 
-N_CRUZA=2             # Número de puntos de cruza
 F = [0.2, 0.4]        # valor entre 0 y 1
 CR = [0.5]            # Probabilidad de cruza binomial: valor entre 0 y 1 
 #---------------------( R E G L A   4 )-------------------------------------#
